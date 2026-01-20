ef9866cdecad0694ce7485cee88fadd127d0455b
ADMIN <ngetichjustine1@gmail.com>
2026-01-20 11:40:39 +0300

    vrs1140

--- app/(pages)/reports/page.tsx
+++ app/(pages)/reports/page.tsx
@@ -2,7 +2,7 @@
 import { auth } from "@/auth";
 import Link from "next/link";
 
-export const revalidate = 1;
+export const dynamic = "force-dynamic";
 
 export default async function ReportsDashboard() {
   const session = await auth();
